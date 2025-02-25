68d862afc8df6a418281ef1903f6a84a0d803193
nasir uddin <nunnasir@gmail.com>
2025-02-25 20:49:56 +0600

    Application Unit Tests

---
+++ tests/TestCommon/Gyms/GymCommandFactory.cs
@@ -0,0 +1,16 @@
+﻿using GymManagement.Application.Gyms.Commands.CreateGym;
+using TestCommon.TestConstants;
+
+namespace TestCommon.Gyms;
+
+public static class GymCommandFactory
+{
+    public static CreateGymCommand CreateCreateGymCommand(
+        string name = Constant.Gym.Name,
+        Guid? subscriptionId = null)
+    {
+        return new CreateGymCommand(
+            Name: name,
+            SubscriptionId: subscriptionId ?? Constant.Subscriptions.Id);
+    }
+}
